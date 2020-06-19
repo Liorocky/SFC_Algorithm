b79c9878677d33f6266604f6c5920efc21be86f8
WarMj <R.Donghui@outlook.com>
2020-06-19 16:31:48 +0800

    剑指 Offer 28. 对称的二叉树  递归

---
+++ LeetCode/SwordToOffer/28-dui-cheng-de-er-cha-shu-lcof/Solution28_1.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null ? true : recur(root.left, root.right);
+    }
+
+    private boolean recur(TreeNode l, TreeNode r) {
+        if (l == null && r == null) return true;
+        if (l == null || r == null || l.val != r.val) return false;
+        return recur(l.left, r.right) && recur(l.right, r.left);
+    }
+}
